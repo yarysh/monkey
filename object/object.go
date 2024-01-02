4d884aa6a7a520a48c45c32ba1e819cd0f46817d
Andrey Yarysh <and@yarysh.com>
2024-01-02 17:45:06 +0100

    [Evaluation] 3.4

---
+++ object/object.go
@@ -0,0 +1,50 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	BOOLEAN_OBJ = "BOOLEAN"
+	INTEGER_OBJ = "INTEGER"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+type Null struct{}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
